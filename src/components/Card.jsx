07cbb6b023749d5a7fb2cdadd26964458eaa6adc
McWooden <garryf7887@gmail.com>
2024-07-07 18:57:04 +0700

    add use sound and implement to btn

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,16 @@
 import QRCode from "react-qr-code";
+import useSound from "use-sound";
 
 export default function Card({item}) {
+    const [play] = useSound(`/hijaiyah/${item.latin}.mp3`)
+    
     return <div className="card bg-base-100 shadow-xl w-full max-w-sm">
         <div className="px-10 pt-10">
-            <p className="bg-secondary rounded-xl p-4 place-items-center text-center text-5xl">{item.arabic}</p>
+            <button className="btn btn-secondary rounded-xl p-4 place-items-center text-center text-5xl w-full h-full" onClick={play}>{item.arabic}</button>
         </div>
-        <div className="card-body items-center text-center flex-row justify-between">
+        <div className="card-body flex flex-row justify-between">
             <h2 className="card-title">{item.latin}</h2>
-            <div className="card-actions">
+            <div className="card-actions flex gap-2 justify-end">
                 <button className="btn btn-primary" onClick={()=>document.getElementById(item.latin).showModal()}>Pindai</button>
             </div>
         </div>
@@ -15,9 +18,9 @@ export default function Card({item}) {
         <div className="modal-box flex flex-col gap-2">
             <h3 className="font-bold text-lg"><span className="bg-secondary p-2 rounded">{item.arabic}</span> {item.latin}</h3>
             <div className="flex place-items-center p-2 justify-center">
-                <div className="p-2 rounded bg-base-100 shadow">
+                <button className="btn p-2 rounded bg-base-100 shadow h-fit" onClick={play}>
                     <QRCode value="latin" level="L"/>
-                </div>
+                </button>
             </div>
         </div>
         <form method="dialog" className="modal-backdrop">
