dcae16272c8a6255f96fd959b736904aeb05364b
McWooden <garryf7887@gmail.com>
2024-07-08 10:26:20 +0700

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,11 +10,11 @@ export default function Hero() {
                 <div className="max-w-xl">
                     <h2 className="text-3xl font-semibold text-secondary-content lg:text-4xl">Belajar Hijaiyah <span className="text-primary">Scan</span></h2>
 
-                    <p className="mt-4 text-sm text-secondary-content lg:text-base">Belajar Hijaiyah sekarang lebih mudah dengan audio. dengarkan suara huruf hijaiyah dengan speaker handphone anda.</p>
+                    <p className="mt-4 text-sm text-secondary-content lg:text-base">Belajar Hijaiyah braille, lebih mudah dengan speaker handphone anda.</p>
 
                     <div className="flex flex-col mt-6 space-y-3 lg:space-y-0 lg:flex-row">
                         <p className="block px-5 py-2 text-sm font-medium tracking-wider text-center text-white transition-colors duration-300 transform bg-primary rounded-md hover:bg-gray-700 cursor-pointer" onClick={() => navigate('/pindai')}>Mulai pindai</p>
-                        <p className="block px-5 py-2 text-sm font-medium tracking-wider text-center text-neutral transition-colors duration-300 transform bg-base-100 rounded-md lg:mx-4 hover:bg-gray-300 cursor-pointer" onClick={() => navigate('/hijaiyah')}>Huruf Hijaiyah</p>
+                        <p className="block px-5 py-2 text-sm font-medium tracking-wider text-center text-neutral transition-colors duration-300 transform bg-base-100 rounded-md lg:mx-4 hover:bg-gray-300 cursor-pointer" onClick={() => navigate('/hijaiyah')}>Hijaiyah braille</p>
                     </div>
                 </div>
             </div>
